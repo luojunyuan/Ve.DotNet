0ba386ec91130ce58185f9ee7d427b1bc1778448
k1mlka <k1mlka@outlook.com>
2021-01-28 17:17:06 +0800

    添加项目文件。

---
+++ Ve.DotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ve.DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
